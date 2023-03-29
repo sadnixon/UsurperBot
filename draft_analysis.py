641244afd6094f9d25959ea5708ac0567d5ff661
sadnixon <askspencerhill@berkeley.edu>
2023-03-29 10:02:50 -0700

    catastrophic point references typo

--- draft_analysis.py
+++ draft_analysis.py
@@ -245,8 +245,8 @@ for i in range(x):
 
             if p_one_grid[j//3][j%3] != 0:
                 if p_one_grid[j//3][j%3].card_id not in ['A0','dA0','N0']:
-                    PR[combo_list[0]][p_one_grid[j//3][j%3].card_id] += p_one_indivs[j]
-                    PRI[combo_list[0]][p_one_grid[j//3][j%3].card_id] += 1
+                    PR[combo_list[1]][p_one_grid[j//3][j%3].card_id] += p_one_indivs[j]
+                    PRI[combo_list[1]][p_one_grid[j//3][j%3].card_id] += 1
 
             for k in range(9):
                 if k >= j+1:
